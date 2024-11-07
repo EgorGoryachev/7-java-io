a791a79afdbd6313990b563b42bb81570f2d41c5
EgorGoryachev <102716070+EgorGoryachev@users.noreply.github.com>
2024-11-08 02:17:12 +0500

    goooooooooooooooooooood

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,20 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        changeFormat();
+    }
+
+    public static void changeFormat() throws IOException {
+        int current = System.in.read();
+        int next;
+
+        while(current != -1){
+            next = System.in.read();
+            if (!(current == 13 && next == 10)){
+                System.out.write(current);
+            }
+            current = next;
+        }
+        System.out.flush();
     }
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,31 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        Sum(System.in);
+    }
 
-        System.out.println("0.0");
+    public static void Sum(InputStream in) {
+        Scanner scanner = new Scanner(new InputStreamReader(in)).useLocale(Locale.US);
+        double sum = 0.0d;
+
+        while (scanner.hasNext()){
+            if(scanner.hasNextDouble()){
+                sum += scanner.nextDouble();
+            }
+            else {
+                scanner.next();
+            }
+        }
+
+        System.out.printf(Locale.US,"%6f", sum);
     }
 }
